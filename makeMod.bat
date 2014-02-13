95a1a48c06b7f6516ffdf13ce0244798c8c3922c
Kubu <szabto@gmail.com>
2014-02-13 19:27:48 +0100

    218 part2

--- makeMod.bat
+++ makeMod.bat
@@ -9,13 +9,13 @@ xcopy promod ..\..\raw\promod /SY
 xcopy maps ..\..\raw\maps /SY
 xcopy mp ..\..\raw\mp /SY
 xcopy shock ..\..\raw\shock /SY
+xcopy xmodel ..\..\raw\xmodel /SY
+xcopy shock ..\..\raw\shock /SY
 xcopy readme.txt ..\..\raw /SY
 copy /Y mod.csv ..\..\zone_source
 cd ..\..\bin
 linker_pc.exe -language english -compress -cleanup mod
-cd ..\Mods\promodlive214_ibj
+cd ..\Mods\PromodLive217
 copy ..\..\zone\english\mod.ff
 
 pause
-cd ..\..\
-iw3mp.exe +set fs_game mods/promodlive214_ibj
